40ffac8605db144c955449f3dcd23226ea78274e
unknown <jair.lopes@fatec.sp.gov.br>
2021-12-22 23:42:42 -0300

    Criado o metodo GET

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 
 const server = express();
+
+server.get('/', (request: Request, response: Response) => {
+
+    // TODAS AS FUNCIONALIDADES
+
+    return response.json({mensagem: 'Bem vindo a nossa API'});
+});
 
 server.listen(5000, () => {
     console.log('Servidor on na porta 5000');
