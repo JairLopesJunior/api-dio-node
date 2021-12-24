987595d2eb607a146c5ff38da5b0b3a78f5e30e8
unknown <jair.lopes@fatec.sp.gov.br>
2021-12-23 22:40:06 -0300

    Criado a rota de usuários

--- index.ts
+++ index.ts
@@ -12,3 +12,14 @@ server.get('/', (request: Request, response: Response) => {
 server.listen(5000, () => {
     console.log('Servidor on na porta 5000');
 });
+
+server.get('/usuarios', (request: Request, response: Response) => {
+    return response.json([
+        {
+            nome: 'Jair'
+        },
+        {
+            nome: 'Joao'
+        }
+    ])
+});
